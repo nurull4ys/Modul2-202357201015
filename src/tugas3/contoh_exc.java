e821da9e13375613b34f81c758d90df08e4c7733
nurul <nurulkhoiyah86@gmail.com>
2024-03-28 18:40:39 +0700

    Exception

--- src/tugas3/contoh_exc.java
+++ src/tugas3/contoh_exc.java
@@ -9,6 +9,22 @@ package tugas3;
  * @author User
  */
 public class contoh_exc {
-    
+    public static void main(String[] args) {
+        int bil = 10;
+        String b[] = {"a","b","c"};
+        try {
+            System.out.println(bil/0);
+            System.out.println(b[3]); 
+        }catch (ArithmeticException ai) {
+            System.out.println("Error Aritmatik");
+            System.out.println(ai.getMessage());
+        }catch (ArrayIndexOutOfBoundsException n) {
+            System.out.println("Error karena melebihi kapasitas array");
+            System.out.println(n.getMessage());
+        }catch (Exception e) {
+            System.out.println("Ada error");
+            System.out.println(e.getMessage());
+        }
+    }
     
 }
